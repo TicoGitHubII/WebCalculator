b929d60217b33355d8d5aa2ef2574dbcbf76a1bd
Tico Clark <ticosemail@gmail.com>
2019-08-12 01:00:07 -0700

    Calc commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebCalculator.Models;
+
+namespace WebCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Operation model)
+        {
+         
+                switch (model.Operator)
+                {
+                    case Operator.Add:
+                        model.Result = model.Left + model.Right;
+                        break;
+                    case Operator.Sub:
+                        model.Result = model.Left - model.Right;
+                        break;
+                    case (Operator.Mul):
+                        model.Result = model.Left * model.Right;
+                        break;
+                    case Operator.Div:
+                        if (model.Right != 0)
+                        {
+                            model.Result = model.Left / model.Right;
+                        }
+                        break;
+                    default:
+                    Console.WriteLine("InValid Operatpor");
+                        break;
+                }
+
+               return View(model);
+            }
+
+
+    }
+
+}
---
+++ Models/Operation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebCalculator.Models
+{
+    public class Operation
+    {
+
+        [Required]
+        [Display(Name = "First Number")]
+        [RegularExpression("(\\+|-)?\\d+", ErrorMessage = "Numbers Only")]
+        public float Left { get; set; }
+
+        [Required]
+        [Display(Name = "Second Number")]
+        [RegularExpression("(\\+|-)?\\d+", ErrorMessage = "Numbers Only")]
+        public float Right { get; set; }
+
+        [Display(Name = "Result Number")]
+        public float Result { get; set; }
+
+
+        [Display(Name = "Operator")]
+        public Operator Operator { get; set; }
+
+
+
+    }
+}
